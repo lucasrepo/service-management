0fd1b0f3f0b30cd0e2479fceef24a56db04bd4a7
celeron <makemeceleron@protonmail.com>
2021-12-09 14:22:41 -0300

    slider

--- views/app.blade.php
+++ views/app.blade.php
@@ -96,9 +96,20 @@
 							       		{{-- FINALIZA LISTADO DE LENGUAJES --}}
 
 							       		@if(null !== $user["img"])
+							       			<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="center: true">
+							       			<ul class="uk-slider-items uk-grid">
 							       			@foreach($user["img"] as $imgUrl)
-							       				<img data-src="img/{{ $imgUrl }}" width="700" height="500" alt="{{ $imgUrl }}" uk-img>
+							       			 <li class="uk-width-3-4">
+									            <div class="uk-panel">
+									                <img data-src="img/{{ $imgUrl }}" alt="{{ $imgUrl }}" uk-img>
+									                {{--<div class="uk-position-center uk-panel"><h1></h1></div>--}}
+									            </div>
+									        </li>
 							       			@endforeach
+							       			</ul>
+							       			<a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+										    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+										</div>
 							       		@endif
 
 							        	</div>
